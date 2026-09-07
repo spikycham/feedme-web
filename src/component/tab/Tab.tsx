681bc95794b1ca0f278933cc85275afa7d454261
Cham <450139220@qq.com>
2026-09-07 22:39:46 +0800

    feat: draw the tab component

---
+++ src/component/tab/Tab.tsx
@@ -0,0 +1,61 @@
+import { ClipboardClock, Hamburger, ListOrdered, UserRoundPen } from "lucide-react";
+import "./index.css";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+interface Tab {
+    key: number;
+    icon: React.ReactNode;
+    title: string;
+    path: string;
+}
+
+const tabs: Tab[] = [
+    {
+        key: 0,
+        icon: <ListOrdered />,
+        title: "Order",
+        path: "/layout/order",
+    },
+    {
+        key: 1,
+        icon: <Hamburger />,
+        title: "Food",
+        path: "/layout/food",
+    },
+    {
+        key: 2,
+        icon: <ClipboardClock />,
+        title: "History",
+        path: "/layout/history",
+    },
+    {
+        key: 3,
+        icon: <UserRoundPen />,
+        title: "Profile",
+        path: "/layout/profile",
+    },
+];
+
+export default function Tab() {
+    const [active, setActive] = useState(0);
+    const navigate = useNavigate();
+
+    return (
+        <div className="tab">
+            {tabs.map((tab) => (
+                <button
+                    key={tab.key}
+                    className={active === tab.key ? "active" : ""}
+                    onClick={() => {
+                        setActive(tab.key);
+                        navigate(tab.path);
+                    }}>
+                    <span>{tab.icon}</span>
+                    <span>{tab.title}</span>
+                    <div className="highlight"></div>
+                </button>
+            ))}
+        </div>
+    );
+}
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,24 @@
 import { Outlet } from "react-router";
 import useInitUser from "@/hook/useInitUser";
 import PageLoading from "@/component/page-loading/PageLoading";
+import Tab from "@/component/tab/Tab";
+import "./index.css";
 
 export default function Layout() {
     const loading = useInitUser();
+    // TODO: navigate to the order page when the user is a merchant.
 
     if (loading) return <PageLoading />;
     return (
-        <div>
-            <div>layout</div>
-            <Outlet />
-        </div>
+        <main className="layout">
+            <section>
+                <div>layout</div>
+                <Outlet />
+            </section>
+
+            <section>
+                <Tab />
+            </section>
+        </main>
     );
 }
