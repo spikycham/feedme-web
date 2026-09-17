b32fa6930a390c2914a10f0b96ef51058391c73d
spikycham <zhangzhengchuan@druid.com>
2026-09-17 15:27:12 +0800

    feat: service worker cache static resources

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+// self.addEventListener("push", (e) => {
+//     const data = e.data?.json() ?? {};
+
+//     e.waitUntil(
+//         self.registration.showNotification(data.title, {
+//             body: data.body,
+//             icon: "/pwa/icon-192.svg",
+//         }),
+//     );
+// });
+
+const CACHE_VERSION = 2;
+
+self.addEventListener("install", (e) => {
+    // e.waitUntil();
+});
+
+self.addEventListener("activate", (e) => {
+    e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+    const req = e.request;
+    
+    // Don't cache.
+    if (req.method !== "GET") return;
+    if (req.url.includes("/api/")) return;
+    if (new URL(req.url).pathname.endsWith("html")) return;
+
+    e.respondWith(
+        caches
+            .match(req)
+            .then(cached => {
+                if (cached) return cached;
+                return fetch(req).then(resp => {
+                    const copy = resp.clone();
+                    caches
+                        .open(String(CACHE_VERSION))
+                        .then(cache => {
+                            cache.put(req, copy);
+                        });
+                });
+            })
+    );
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,21 @@
-import { useEffect } from "react";
-import { RouterProvider } from "react-router";
-import { router } from "@/route";
+import { RouterProvider } from 'react-router'
+import { router } from '@/route'
 
-import Cover from "@/view/cover/Cover";
-import Message from "@/component/message/Message";
-import fetchSubscription from "./network/subscription.api";
+import Cover from '@/view/cover/Cover'
+import Message from '@/component/message/Message'
 
-const VAPID_PUBLIC_KEY =
-    "BJE0T6g_aie9OY9ImL3JJexBHZiX-_rjfk1i3jSQH6ppidd1a7FO8ux5wupYAL5jMC8m-_RMnkkmPRTV-59dxQY";
-function urlBase64ToUint8Array(base64: string): Uint8Array<ArrayBuffer> {
-    const padding = "=".repeat((4 - (base64.length % 4)) % 4);
-    const base64String = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
-
-    const rawData = atob(base64String);
-    const buffer = new ArrayBuffer(rawData.length);
-    const output = new Uint8Array(buffer);
-
-    for (let i = 0; i < rawData.length; i++) {
-        output[i] = rawData.charCodeAt(i);
-    }
-
-    return output;
+if (navigator.serviceWorker) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' })
+    })
 }
 
 export default function App() {
-    useEffect(() => {
-        const initNotification = async () => {
-            const permission = await Notification.requestPermission();
-            if (permission !== "granted") {
-                return;
-            }
-
-            await navigator.serviceWorker.register("/pwa/sw.js");
-            const registration = await navigator.serviceWorker.ready;
-            const subscription = await registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
-            });
-            await fetchSubscription(subscription);
-        };
-
-        initNotification();
-    }, []);
-
     return (
         <>
             <Cover />
             <Message />
             <RouterProvider router={router} />
         </>
-    );
+    )
 }
